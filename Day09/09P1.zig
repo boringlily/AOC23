b0b4cdec6972867d41eba7521cd2a5ee171f8468
Lily Nikitin <53998902+boringlily@users.noreply.github.com>
2023-12-10 22:03:05 -0800

    Started Day9

---
+++ Day09/09P1.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const print = std.debug.print;
+
+const demo_input = @embedFile("demo.txt")
+
+
+fn solve() u32 {}
+
+pub fn main() !void {}
+
+test "demo" {}
